b90cdd77979f7e64b8158d283e280e09e2d6765a
Brenno Ogeda <brennoogeda@gmail.com>
2016-03-04 15:35:35 -0300

    Alteração da página inicial do projeto.

--- imobiliaria/index.php
+++ imobiliaria/index.php
@@ -1,1 +1,11 @@
-<?php
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8" />
+	<title>Projeto Imobiliária</title>
+</head>
+<body>
+	<h1>Projeto Imobiliároia</h1>
+	<p>Alterações no projeto</p>
+</body>
+</html>
